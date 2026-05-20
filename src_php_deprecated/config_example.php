dde75d0ef8bde679c76ca86fa374503c62cc7d5c
Sergey Karmanov <ppoocpel8888@gmail.com>
2026-05-21 00:01:43 +0300

    Удалил php наследие

--- src_php_deprecated/config_example.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'url' => 'http://www.tgnvoda.ru/avarii.php',
-    // Telegram
-    'Send_silent' => 'false',
-    'Telegram_token' => 'token',
-    'Telegram_channel' => '@channel'
-];
-?>
